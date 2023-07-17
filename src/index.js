b65f033b1618af6be8651b2f6605d12d4a9d23c0
ольга скворцова <olya21062003@gmail.com>
2023-07-17 04:34:10 +0300

    feat: made adaptive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import Swiper from 'swiper';
 import './index.html';
 import './index.sass';
-import { menu, burger, shadow, navigation, burgerLines, head, body, swiperReadMore, swiperContainer, swiperWrapper, swiperContainerService } from './modules/calc';
+import { menu, burger, shadow, navigation, burgerLines, head, body, swiperReadMore, swiperContainer, swiperWrapper } from './modules/calc';
 
 burger.addEventListener('click', changeColor);
 swiperReadMore.addEventListener('click', readMoreAvailable);
@@ -15,8 +16,15 @@ function changeColor() {
     head.classList.toggle('absolute-burger');
 }
 
+function initServiceSwiper() {
+    const serviceSwiper = new Swiper('.services__swiper-container', {
+        spaceBetween: 19.5,
+        freeMode: true,
+        slidesPerView: 1.25,
+    });
+}
 function initSlider() {
-    new Swiper('.swiper-container', {
+    const swiper= new Swiper('.swiper-container', { /* если не работает, убрать до new */
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
@@ -25,16 +33,31 @@ function initSlider() {
         spaceBetween: 16,
         centeredSlides: true,
     });
+    const priceSwiper = new Swiper('.swiperprice__container', {
+        slidesPerView: 1,
+        spaceBetween: 16,
+        centeredSlides: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 }
 
 
 function detectDevice() {
     if(document.documentElement.clientWidth < 750){
-      initSlider();
-    }
+        initSlider();
+    };
 }
 detectDevice();  
 
+function detectDevice2() {
+    if(document.documentElement.clientWidth < 1103) {
+        initServiceSwiper();
+    }
+}
+detectDevice2();
 
 function readMoreAvailable () {
     if(document.documentElement.clientWidth > 750 && document.documentElement.clientWidth < 1090){
@@ -54,5 +77,5 @@ function readMoreAvailable () {
         swiperReadMore.firstElementChild.classList.toggle('imgtransform');
         swiperReadMore.lastElementChild.classList.toggle('displaynone');
         swiperReadMore.children[1].classList.toggle('flexdisplay');
-    }
-}
+    };
+};
